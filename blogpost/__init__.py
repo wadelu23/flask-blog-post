9f25cf3591cdc1fe0a0f71289221d29ac3b6444e
wadelu23 <wadelu23@gmai.com>
2021-10-15 17:08:23 +0800

    [feature] add error page handler
    add  app_errorhandler for 403 and 404
    add html file

--- blogpost/__init__.py
+++ blogpost/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from blogpost.config import config
 from blogpost.core.views import core
+from blogpost.error_pages.handlers import error_pages
 
 app = Flask(__name__)
 
@@ -10,3 +11,4 @@ flask_env = os.environ.get('FLASK_ENV') or 'default'
 app.config.from_object(config[flask_env])
 
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
