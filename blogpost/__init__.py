513e6e69b3534fd63551194f00db180d3b889bdf
wadelu23 <wadelu23@gmai.com>
2021-10-23 15:47:13 +0800

    [feature] add post view/form
    1. view all post in index
    2. view the user's all post in the user's page
    3. use flask_simplemde and markdown filter to create post
    4. update or delete post with permission

--- blogpost/__init__.py
+++ blogpost/__init__.py
@@ -6,7 +6,10 @@ from blogpost.login_manager import login_manager
 from blogpost.config import config
 from blogpost.core.views import core
 from blogpost.users.views import users
+from blogpost.blog_posts.views import blog_posts
 from blogpost.error_pages.handlers import error_pages
+from flask_simplemde import SimpleMDE
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
 
@@ -18,6 +21,10 @@ app.config.from_object(config[flask_env])
 db.init_app(app)
 Migrate(app, db)
 
+app.config['SIMPLEMDE_JS_IIFE'] = True
+app.config['SIMPLEMDE_USE_CDN'] = True
+SimpleMDE(app)
+Markdown(app, extensions=['fenced_code', 'tables'])
 
 # Login Config
 login_manager.init_app(app)
@@ -26,4 +33,5 @@ login_manager.login_view = 'users.login'
 
 app.register_blueprint(core)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
 app.register_blueprint(error_pages)
